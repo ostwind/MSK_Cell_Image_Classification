37b00f0bc96c12da5575a059e8b1b288cc463959
ostwind <lihan.yao@gmail.com>
2017-07-16 15:22:32 -0400

    zoom and label functionality, streamlined preprocess

--- src/main.py
+++ src/main.py
@@ -9,97 +9,165 @@ from tifffile import TiffFile, imsave, imread
 import itertools
 
 dir = os.path.normpath(os.getcwd() + os.sep + os.pardir)
-data = os.path.join(dir, 'data/')
+data = os.path.join(dir, 'data/display/')
+zoom = os.path.join(dir, 'data/zoom/')
 
-window = Tk()
-window.geometry('2000x1000+0+0')
-window.title("")
+class MainApplication(Frame):
+    def __init__(self, parent, *args, **kwargs):
+        Frame.__init__(self, parent, *args, **kwargs)
+        self.parent = parent
 
-#window.config(background = 'white')
+        self.queue_pos = 0
+        self.queue = self.fill_queue(data)
+        self.zoom_queue = self.fill_queue(zoom)
 
-batch_to_display = []
+        ''' here we store user input
+            knows: 0 if sample is unknwn, 1 if sample is knwn (default)
+            labels: 0 if sample is not tumor (defaul), 1 if sample is tumor
+            zooms: 0 if not zoomed (default), 1 if zoomed
+        '''
+        self.knows = [1 for x in self.queue]
+        self.labels = [0 for x in self.queue ]
+        self.zooms = [0 for x in self.queue ]
 
-for dirpath, dirs, files in os.walk(data):
-    for f in files:
-        if '.png' in f:
+        self.populate()
 
-            batch_to_display.append(dirpath+'%s'%(f))
+    def fill_queue(self, path):
+        filled_queue = []
+        for dirpath, dirs, files in os.walk(path):
+            for f in files:
+                if '.png' in f:
+                    filled_queue.append(dirpath+'%s'%(f))
+        return filled_queue
 
-            #break
-#image = imread(data + 'B7H3_AFRemoved_pyr16_spot_004.tif')
-#matrix = np.array(image, dtype = np.uint16)
-#im = Image.fromarray(matrix)
+    def place_sample(self, panel, side = LEFT):
 
-def place_sample(img_index, panel, side = LEFT):
-    im = Image.open(  batch_to_display[img_index] )
-    im = im.resize((420+80, 240+26), Image.ANTIALIAS )
-    tkimage = ImageTk.PhotoImage(im)
-    imvar = Label(panel, image = tkimage)
-    imvar.image = tkimage
-    imvar.pack(side = side)
+        print(self.queue_pos, len(self.queue))
+        im = Image.open(  self.queue[self.queue_pos] )
+        im = im.resize((420+80, 240+26), Image.ANTIALIAS )
+        tkimage = ImageTk.PhotoImage(im)
+        imvar = Label(panel, image = tkimage)
+        imvar.image = tkimage
+        imvar.pack(side = side)
 
-    input_panel = Frame(panel)
-    #input_panel.config(background= 'white')
+        input_panel = Frame(panel)
 
-    var = IntVar()
-    tumor= Checkbutton(input_panel, text= 'tumor', variable = var)
-    tumor.pack(side = TOP )
+        t_var = IntVar()
+        tumor= Checkbutton(
+        input_panel, text= 'tumor', variable = t_var, command = lambda: self.flick(
+        self.pos(t_var)))
+        tumor.var = t_var
+        tumor.pack(side = TOP )
 
-    var = IntVar()
-    unknown= Checkbutton(input_panel, text= 'unkwn', variable = var)
-    unknown.pack(side = TOP )
-    input_panel.pack(side = side)
+        u_var = IntVar()
+        unknown= Checkbutton(
+        input_panel, text= 'unkwn', variable = u_var, pady = 20, padx = 20,
+        command = lambda: self.flick(
+        self.pos(u_var, offset = 1), array='knows'))
+        unknown.var = u_var
+        unknown.pack(side = TOP )
 
-num_panels = len(batch_to_display) // 3
-left_overs = len(batch_to_display) % 3
+        z_var = IntVar()
+        zoom = Button(
+        input_panel, text='zoom/unzoom', command = lambda: self.flick(
+        self.pos(z_var, offset = 2) , image_var = imvar, array = 'zooms'))#, pady = 50, padx = 30)
+        zoom.var = z_var
+        zoom.pack(side = BOTTOM)
 
-for row in range( num_panels):
-    sample_panel = Frame(window)
-    #sample_panel.config(background = 'white')
+        input_panel.pack(side = side)
 
-    place_sample( 3*row, sample_panel)
-    place_sample( 3*row + 1, sample_panel)
-    place_sample( 3*row+2, sample_panel )
+    def pos(self, var, offset = 0):
+        var = str(var)
+        var = ''.join(i for i in var if i.isdigit())
+        print(var)
+        return int( (int(var) - offset) /3)
 
-    sample_panel.pack(side = TOP)
+    def flick(self, pos, image_var = '', array = 'labels'):
+        edit_queue = eval('self.' + array)
+        print(pos, len(edit_queue))
+        if edit_queue[pos] == 0:
+            edit_queue[pos] = 1
+        else:
+            edit_queue[pos] = 0
+        if image_var:
+            self.zoom_func(image_var, pos, edit_queue[pos])
+        print(array, pos,  edit_queue)
 
-if left_overs != 0:
-    sample_panel = Frame(window)
-    place_sample( len(batch_to_display) -1, sample_panel)
-    if left_overs == 2:
-        place_sample( len(batch_to_display) -2 , sample_panel)
-    sample_panel.pack(side= TOP)
+    def place_row(self):
+        sample_panel = Frame(window)
 
-conclude_panel = Frame(window)
+        if self.queue_pos < len(self.queue):
+            self.place_sample(sample_panel)
+            self.queue_pos += 1
+            while self.queue_pos % 3 != 0 and self.queue_pos < len(self.queue):
+                self.place_sample( sample_panel)
+                self.queue_pos += 1
+
+        sample_panel.pack(side = TOP)
+
+    def zoom_func(self, imvar, pos, zoom = 0):
+        if zoom:
+            self.update_img( imvar, self.zoom_queue[0] )
+        else:
+            self.update_img( imvar, self.queue[0] )
+
+    def update_img(self, imvar, path):
+        im = Image.open(path)
+        im = im.resize((420+80, 240+26), Image.ANTIALIAS )
+        tkimage = ImageTk.PhotoImage(im)
+        imvar.configure(image = tkimage)
+        imvar.image = tkimage
+        imvar.pack(side = LEFT)
+
+    def populate( self, imgs_per_page = 9 ):
+        l = window.pack_slaves()
+        if l:
+            for item in l:
+                item.destroy()
+
+        num_rows = imgs_per_page // 3
+        left_overs = imgs_per_page % 3
+
+        while self.queue_pos < len(self.queue):
+            self.place_row()
+        ####################
+
+        conclude_panel = Frame(window)
+
+        submit = Button(
+        conclude_panel, text='submit', pady = 50, padx = 50,
+        command = lambda: self.update() )
+        submit.pack(side = LEFT)
+
+        quit = Button(conclude_panel, text='quit', pady = 50, padx = 50, command = window.quit)
+        quit.pack(side= RIGHT)
+
+        conclude_panel.pack(side=TOP)
+
+if __name__== "__main__":
+
+    window = Tk()
+    window.geometry('2000x1000+0+0')
+    window.title("Tumor-or-Not v1.0")
+
+    MainApplication(window).pack(side='top', fill = 'both', expand = True)
+
+    #window.bind("<Return>", MainApplication.update)
+    window.mainloop()
+
+####################
 
 
-zoom = Button(conclude_panel, text='zoom / unzoom', pady = 75, padx = 50)
-zoom.pack(side = LEFT)
-
-submit = Button(conclude_panel, text='submit', pady = 75, padx = 75)
-submit.pack(side = LEFT)
-
-quit = Button(conclude_panel, text='quit', pady = 75, padx = 75, command = window.quit)
-quit.pack(side= RIGHT)
-
-conclude_panel.pack(side=TOP)
-
-window.mainloop()
 
 
 
 
 
-def gen_coord():
-    grid = list(range(3))
-    all_coordinates = grid*3
-    i = -1
-    for pos in all_coordinates:
-        if pos == 0:
-            i += 1
-        yield pos, i
-
-# def open_jpg(path):
-#     jpg_to_tkinter = Image.open(path)
-#     jpg_to_tkinter = jpg_to_tkinter.resize((210, 120), Image.ANTIALIAS)
-#     return ImageTk.PhotoImage( jpg_to_tkinter)#, size=(3,3)  )
+# def gen_coord():
+#     grid = list(range(3))
+#     all_coordinates = grid*3
+#     i = -1
+#     for pos in all_coordinates:
+#         if pos == 0:
+#             i += 1
+#         yield pos, i
