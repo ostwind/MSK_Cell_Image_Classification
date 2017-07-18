2045be75ec6c96ea54f7ba108a8f7a7fa35aafea
ostwind <lihan.yao@gmail.com>
2017-07-18 11:22:40 -0400

    added file i/o and zoom

--- src/main.py
+++ src/main.py
@@ -18,8 +18,11 @@ class MainApplication(Frame):
         self.parent = parent
 
         self.queue_pos = 0
-        self.queue = self.fill_queue(data)
-        self.zoom_queue = self.fill_queue(zoom)
+        self.last_visited = 0
+        self.queue = []
+        self.zoom_queue = []
+        self.fill_queues()
+
         ''' here we store user input
             knows: 0 if sample is unknwn, 1 if sample is knwn (default)
             labels: 0 if sample is not tumor (defaul), 1 if sample is tumor
@@ -29,22 +32,28 @@ class MainApplication(Frame):
         self.labels = [0 for x in self.queue ]
         self.zooms = [0 for x in self.queue ]
 
+        self.image_dim = [500, 250]
         self.pg = 0
+        self.last_pg = ''
+        self.label_check_pointers = []
+        self.know_check_pointers = []
         self.image_pointers = []
+
+        self.label_storage_path = os.getcwd() + '/' + 'labels.txt'
         self.populate()
 
-    def fill_queue(self, path):
-        filled_queue = []
+        self.import_labels()
+
+    def fill_queues(self, path = data, zoom_path = zoom):
         for dirpath, dirs, files in os.walk(path):
             for f in files:
                 if '.png' in f:
-                    filled_queue.append(dirpath+'%s'%(f))
-        return filled_queue
+                    self.queue.append(path +f)
+                    self.zoom_queue.append(zoom_path + f )
 
     def place_sample(self, panel, side = LEFT):
-
         im = Image.open( self.queue[self.queue_pos] )
-        im = im.resize((420+80, 240+26), Image.ANTIALIAS )
+        im = im.resize((self.image_dim[0], self.image_dim[1]), Image.ANTIALIAS )
         tkimage = ImageTk.PhotoImage(im)
         imvar = Label(panel, image = tkimage)
         self.image_pointers.append(imvar)
@@ -56,43 +65,34 @@ class MainApplication(Frame):
         t_var = IntVar()
         tumor= Checkbutton(
         input_panel, text= 'tumor', variable = t_var, pady = 20, padx = 20,
-        command = lambda: self.flick(
-        self.pos(t_var)))
+        command = lambda: self.check_write(
+        tumor))
         tumor.var = t_var
         tumor.pack(side = TOP )
+        self.label_check_pointers.append( tumor )
 
         u_var = IntVar()
         unknown= Checkbutton(
         input_panel, text= 'unkwn', variable = u_var, pady = 20, padx = 20,
-        command = lambda: self.flick(
-        self.pos(u_var, offset = 1), array='knows'))
+        onvalue = 0, offvalue = 1,
+        command = lambda: self.check_write(unknown , array='knows'))
         unknown.var = u_var
+        unknown.deselect()
         unknown.pack(side = TOP )
+        self.know_check_pointers.append(unknown)
+
+        caption = Label(input_panel, pady=20, text= 'spot ' + self.queue[self.queue_pos].split('/')[-1].split('_')[0] )
+        caption.pack(side = BOTTOM)
 
         z_var = IntVar()
         zoom = Button(
-        input_panel, text='zoom', command = lambda: self.flick(
-        self.pos(z_var, offset = 2) , image_var = imvar, array = 'zooms'))#, pady = 50, padx = 30)
+        input_panel, text='zoom',
+        command = lambda: self.check_write(
+        zoom, image_var = imvar, array = 'zooms') )#, pady = 50, padx = 30)
         zoom.var = z_var
         zoom.pack(side = BOTTOM)
 
         input_panel.pack(side = side)
-
-    def pos(self, var, offset = 0):
-        var = str(var)
-        var = ''.join(i for i in var if i.isdigit())
-        return int( (int(var) - offset) /3) + (self.pg * 9)
-
-    def flick(self, pos, image_var = '', array = 'labels'):
-        edit_queue = eval('self.' + array)
-        #print(pos, len(edit_queue))
-        if edit_queue[pos] == 0:
-            edit_queue[pos] = 1
-        else:
-            edit_queue[pos] = 0
-        if image_var:
-            self.zoom_func(image_var, pos, edit_queue[pos])
-        print(array, pos,  edit_queue)
 
     def place_row(self):
         sample_panel = Frame(window)
@@ -106,58 +106,160 @@ class MainApplication(Frame):
 
         sample_panel.pack(side = TOP)
 
+    def pos(self, var, offset = 0):
+        var = str(var)
+        var = ''.join(i for i in var if i.isdigit())
+        position = int( (int(var) - offset) /3) + (self.pg * 9)
+        # user may quit halfway, assume everything upto the last bit manipulated
+        # is actually valid to export, see self.export()
+        if position > self.last_visited:
+            self.last_visited = position
+        return position
+
+    def uncheck_all(self):
+        for c in self.label_check_pointers:
+            c.deselect()
+        for d in self.know_check_pointers:
+            d.deselect()
+
+    def read_check(self, pos = '', checkbutton = ''):
+        ''' select checkbuttons on a new page
+            :param pos: if given, checks UI box according to image position in queue
+            :param button: if given, checks UI box according to box's location
+        '''
+        if checkbutton:
+            pos = self.pos(checkbutton.var)
+        else:
+            checkbutton = self.label_check_pointers[pos % 9]
+            knows_checkbutton = self.know_check_pointers[pos % 9]
+        if pos < len(self.queue):
+            # if unknwn button checked from prev page, check it now
+            if self.knows[pos] == 0:
+                knows_checkbutton.select()
+            # if tumor is checked from prev page, check it now
+            if self.labels[pos] == 1:
+                #print('checkbutton checked at', pos, self.labels[pos])
+                checkbutton.select()
+
+    def check_write(self, checkbutton, image_var = '', array = 'labels'):
+        ''' reads checkbutton states and writes them to self.knows, self.labels
+            :param checkbutton: box to get value from
+            :param image_var: if given, zoom image_var's image
+            :param array: determine which array to write to (self.knows, self.labels)
+        '''
+        edit_queue = eval('self.' + array)
+        pos = self.pos(checkbutton.var)
+
+        if image_var:
+            # zoom is a button. Instead of reading state, manually 0/1 flip
+            edit_queue[ pos ] = 1 - edit_queue[ pos ]
+            self.zoom_func(image_var, pos, edit_queue[pos])
+            return
+        edit_queue[ pos ] = checkbutton.var.get()
+        #print(array, edit_queue, pos)
+
     def zoom_func(self, imvar, pos, zoom = 0):
         if zoom:
-            self.update_img( imvar, self.zoom_queue[0] )
+            self.update_img( imvar, self.zoom_queue[pos] )
         else:
             self.update_img( imvar, self.queue[pos] )
 
     def update_img(self, imvar, path):
         im = Image.open(path)
-        im = im.resize((420+80, 240+26), Image.ANTIALIAS )
+        im = im.resize((self.image_dim[0], self.image_dim[1]), Image.ANTIALIAS )
         tkimage = ImageTk.PhotoImage(im)
         imvar.configure(image = tkimage)
         imvar.image = tkimage
         imvar.pack(side = LEFT)
 
-    def update(self, next_page_range ):
+    def update(self, prev = False ):
+        """ Loads new page. Verifies page turning is possible, unchecks boxes,
+            loads next image in the slot or a blank
+            :param prev: bit to determine if next page or prev page, sets appropriate
+            interval of images to traverse (self.pg * 9) in queue
+        """
+        if prev:
+            if self.pg == 0:
+                return
+            self.pg -= 1
+
+        else:
+            if self.last_pg == self.pg:
+                return
+            self.pg += 1
+        self.uncheck_all()
         blank = os.path.join(dir, 'data/util/pixel.png')
-        for pointer in self.image_pointers:
-            self.update_img(pointer, blank)
 
-        self.pg += 1
-
-        while self.queue_pos != next_page_range:
+        self.queue_pos = self.pg * 9
+        while self.queue_pos != (self.pg + 1) * 9:
             for pointer in self.image_pointers:
-                if self.queue_pos == len(self.queue):
-                    return
-                #print(self.queue_pos, next_page_range, self.queue_pos < len(self.queue) )
-                self.update_img(pointer, self.queue[self.queue_pos] )
+                if self.queue_pos >= len(self.queue):
+                    self.last_pg = self.pg
+                    img_up_next = blank
+                else:
+                    img_up_next = self.queue[self.queue_pos]
+                self.update_img(pointer, img_up_next )
+                self.read_check(pos = self.queue_pos, checkbutton = ''  )
+                #print(self.queue_pos % 9, len(self.labels), self.queue_pos, self.labels[self.queue_pos])
                 self.queue_pos += 1
 
-
     def populate( self, imgs_per_page = 9 ):
-        #num_rows = imgs_per_page // 3
-        #left_overs = imgs_per_page % 3
-        #next_page_range = self.queue_pos + imgs_per_page
-        # stop if last image in directory, or out of display space
-        #while self.queue_pos < len(self.queue) and self.queue_pos < next_page_range:
         for i in range(3):
             self.place_row()
-        ####################
 
         conclude_panel = Frame(window)
+        prev_ = Button(
+        conclude_panel, text='prev', pady = 50, padx = 50,
+        command = lambda: self.update( prev = True))
+        prev_.pack(side = LEFT)
+
+        next_ = Button(
+        conclude_panel, text='next', pady = 50, padx = 50,
+        command = lambda: self.update() )
+        next_.pack(side = LEFT)
 
         submit = Button(
-        conclude_panel, text='submit', pady = 50, padx = 50,
-        command = lambda: self.update(self.queue_pos + imgs_per_page) )
+        conclude_panel, text='submit', pady = 50, padx = 40,
+        command = lambda: self.export_labels() )
         submit.pack(side = LEFT)
 
-        quit = Button(conclude_panel, text='quit', pady = 50, padx = 50, command = window.quit)
+        instr = Label(conclude_panel, pady = 50,
+        text = 'press submit to save \n labels stored at \n %s' %(self.label_storage_path))
+        instr.pack(side = RIGHT)
+
+        quit = Button(conclude_panel, text='quit', pady = 50, padx = 40,
+        command = window.quit)
         quit.pack(side= RIGHT)
 
-        conclude_panel.pack(side=TOP)
+        conclude_panel.pack(side=TOP, pady = 20)
 
+    def export_labels(self):
+        label_dict = dict()
+        self.labels = [x if x != 0 else x - 1 for x in self.labels ]
+        for i in range(self.last_visited+1):
+            label_dict[ self.queue[i] ] = self.labels[i] * self.knows[i]
+        with open(self.label_storage_path, 'w') as f:
+            f.write( str(label_dict) )
+
+    def import_labels(self):
+        if not os.path.isfile(self.label_storage_path):
+            with open(self.label_storage_path, 'w') as e:
+                e.write( 'dict()')
+
+        with open(self.label_storage_path, 'r') as f:
+            label_dict = eval(f.read())
+
+        for i in range(len(self.queue)):
+            if self.queue[i] in label_dict.keys():
+                #print('pos at _ have value _ ', i, label_dict[self.queue[i]])
+                if label_dict[self.queue[i]] == 0:
+                    self.knows[i] = 0
+                if label_dict[self.queue[i]] == 1:
+                    self.labels[i] = 1
+                if label_dict[self.queue[i]] == -1:
+                    self.labels[i] = 0
+                if i < 9:
+                    self.read_check(pos = i)
 if __name__== "__main__":
 
     window = Tk()
@@ -166,11 +268,12 @@ if __name__== "__main__":
 
     MainApplication(window).pack(side='top', fill = 'both', expand = True)
 
-    #window.bind("<Return>", MainApplication.update)
     window.mainloop()
 
-####################
-
+# TODO
+# color key
+# user chosen proteins
+# make zoom images
 
 
 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -3,19 +3,28 @@
     takes original GE greyscales and generates GUI ready images
 '''
 
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image, ImageEnhance, ImageFilter
 from tifffile import TiffFile, imsave, imread
 import numpy as np
 import os
 from scipy import misc
 
 dir = os.path.normpath(os.getcwd() + os.sep + os.pardir +'/data')
-original = os.path.join(dir, 'original')
-colored = os.path.join(dir, 'colored')
+original = os.path.join(dir, 'original/')
+colored = os.path.join(dir, 'colored/')
 cropped = os.path.join(dir, 'cropped/')
-print('Final output saved to %s' %(cropped) )
+print('Final output saved to %s' %(colored) )
 
-def colorized(file_paths = original, output_path = colored):
+def zoom(input_directory = os.path.join(dir, 'display/'),
+    output_directory = os.path.join(dir, 'zoom/')):
+    for subdir, dirs, files in os.walk(input_directory):
+        for f in files:
+            img = Image.open(input_directory + f)
+            img = img.transform((500, 200), Image.EXTENT, (125, 50, 250, 100 ))
+            img.save(output_directory + f)
+zoom()
+
+def colorized(file_paths, output_path = colored):
     ''' given a list of tif file paths, normalize underlying matrices wrt
         pixel intensity and experimental distribution of markers, then fuse
         multiple greyscale into a (8int, 8int, 8int) RGB additive format
@@ -23,6 +32,7 @@ def colorized(file_paths = original, output_path = colored):
     matrix_stack = []
 
     for path in file_paths:
+        print(path)
         matrix_stack.append( np.array(Image.open(path)) )
 
     weights = [256-150, 256-150, 256]
@@ -32,19 +42,28 @@ def colorized(file_paths = original, output_path = colored):
         RGB[:,:,i] = matrix_stack[i]/weights[i]
 
     img = Image.fromarray(RGB)
-    img.resize((840,480), Image.ANTIALIAS)
-    img.save( os.path.join(output_path, 'rgb_AA.png'), 'PNG')
 
-filenames = ['MELANA_AFRemoved_pyr16_spot_004.tif',
-'CD4_AFRemoved_pyr16_spot_004.tif',
-'S001_mono_dapi_reg_pyr16_spot_004.tif']
+    img.resize((7082,4424), Image.ANTIALIAS)
+    img_name = file_paths[0].split('.')[0].split('/')[-1]
+    img.save( os.path.join(output_path, img_name +'.png'), 'PNG' )
 
-file_paths = []
-for f in filenames:
-    file_paths.append( os.path.join(original, f) )
+def yield_rgb_triplet(input_directory = cropped):
+    all_file_names = []
+    for subdir, dirs, files in os.walk(input_directory):
+        for f in files:
+            all_file_names.append(f)
 
-colorized( file_paths, output_path = colored )
-print('colorized!')
+    while all_file_names:
+        key = '_'.join( all_file_names[0].split('_')[2:] )
+        triplet = [ f for f in all_file_names if key in f ]
+        rgb_order = ['SOX10', 'CD4', 'S001']
+        rgb_triplet = [ f for i in range(3) for f in triplet if rgb_order[i] in f ]
+
+        all_file_names = [f for f in all_file_names if f not in rgb_triplet]
+        yield rgb_triplet
+
+# for t in yield_rgb_triplet():
+#     colorized( [cropped + t[0], cropped + t[1], cropped + t[2]] )
 
 def yield_coord(image, shift = [0, 0], stride = [400, 200], axis = 'x'):
     height, width = (4400, 7000)
@@ -55,16 +74,24 @@ def yield_coord(image, shift = [0, 0], stride = [400, 200], axis = 'x'):
         for y in range(0, height, stride[1]):
             yield y + shift[1], y+ shift[1] +stride[1]
 
-def grid_crop(input_directory = colored, output_directory = cropped):
+def grid_crop(input_directory = original, output_directory = cropped):
     for subdir, dirs, files in os.walk(input_directory):
         for f in files:
-            image = Image.open(f)
+            image = Image.open(input_directory + f)
             #print(f)
-            if '.png' in f:
+            if '.tif' in f:
                 for y1, y2 in yield_coord( f, axis = 'y'):
                     for x1, x2 in yield_coord( f):
                         small_img = image.crop((x1, y1, x2, y2))
-                        save_name = '%s_%s_%s_%s_%s.png' %(f.split('_')[0], x1, x2, y1, y2)
+                        save_name = '%s_%s_%s_%s_%s_%s.tif' %(
+                        f.split('_')[-1][:3], f.split('_')[0], x1, x2, y1, y2)
                         small_img.save(output_directory + save_name)
-
-grid_crop()
+#grid_crop()
+#
+# file_paths = []
+# for subdir, dirs, files in os.walk(original):
+#     for f in files:
+#         file_paths.append( os.path.join(original, f) )
+#
+# colorized( file_paths, output_path = colored )
+# print('colorized!')
