05a6c91e512e0595f5d850f11a7ed9aacd3ba113
ostwind <lihan.yao@gmail.com>
2017-07-29 17:01:02 -0400

    updated with scrollbar and filename scheme

--- src/HALO_util/afi.py
+++ src/HALO_util/afi.py
@@ -28,10 +28,16 @@ def case_name(path):
     parent_dir = path.split('/')[-2]
     if '_' not in parent_dir:
         return parent_dir
-    return parent_dir.split('_')[0] + '_' + parent_dir.split('_')[1]
+    return parent_dir #parent_dir.split('_')[0] + '_' + parent_dir.split('_')[1]
 
 def channel_name(file_name):
-    cname = file_name.split('_')[0]
+    file_name_in_list = file_name.split('_')
+    cname = file_name_in_list[0]
+
+    if len(file_name_in_list[1]) < 3:
+        cname += '_' + file_name_in_list[1]
+    if  len(file_name.split('_')[2]) < 3:
+        cname += '_' + file_name_in_list[2]
     if 'dapi' in file_name:
         return 'DAPI%s'%( int(cname[1:]) )
     return cname
@@ -91,13 +97,19 @@ def traverse(start_dir, mask_dir, num_stains):
         @param None or str num_stains: GUI field 3, prints warning if spot does not contain this many files
         @return dic tif_ledger: an empty tif_ledger indicates no TIF were found
     '''
+
     tif_ledger = dict()
     for dirpath, dirs, files in os.walk(start_dir):
         dirpath = os.path.join(dirpath, '')
         tif_ledger = collect_spots(dirpath)
         for spot in tif_ledger.keys():
-                if num_stains and len(tif_ledger[spot]) != int(num_stains):
-                    update_usr('Spot %s at %s has %s .tif files, not expected' %(spot, dirpath, len(tif_ledger[spot])))
+                if len(tif_ledger[spot]) != int(num_stains):
+                    update_usr(listbox, 'Spot %s at %s has %s .tif files, no .afi written' %(spot, dirpath, len(tif_ledger[spot])))
+                    update_usr(listbox, '!! no .afi written !!' )
+                    continue
+                if  len(tif_ledger[spot]) == int(num_stains):
+                    update_usr(listbox, 'Spot %s at %s has %s .tif files' %(spot, dirpath, len(tif_ledger[spot])))
+
                 write_afi(dirpath, mask_dir, spot, tif_ledger)
     return tif_ledger
 
@@ -105,7 +117,7 @@ def path_exists(path):
     return os.path.exists(os.path.dirname(path))
 
 master = Tk()
-master.geometry('1000x500+300+300')
+master.geometry('1200x500+300+300')
 master.title("HALO's TIFF Linker")
 
 top_label = Label(master, text="Root Directory \n to Traverse" )
@@ -123,30 +135,6 @@ bot_label.config(font=('Courier',12))
 e1 = Entry(master, textvariable=1, width=80)
 e1.grid(row=0, column=1, pady = 20, padx = 10)
 
-def show_entry_fields():
-   ''' calls afi writing functions according to user input into GUI
-        e1 corresponds to first field, e2 to second field, so on.
-        traverse( ) takes all user inputs and executes the task, also serves as boolean
-        in case directory does not contain any .tif
-   '''
-   path = os.path.join(e1.get(), '')
-   if not path_exists(path):
-       update_usr('Not a valid directory path')
-       return
-
-   mask_dir = e2.get() #os.path.join(e2.get(), '')
-   print(mask_dir)
-
-   num_stains = (e3.get())
-   if num_stains and not num_stains.isdigit():
-       update_usr('Number of Stains not an integer')
-       return
-
-   # file written by link after calling it, link also a bool indicating if tiff found at path
-   if not traverse(path, mask_dir, num_stains):
-       update_usr('No .tif or .tiff found anywhere in %s' %(path))
-       return
-   update_usr('%s written to %s' %('filename', path) )
 
 e2 = Entry(master, width = 80)
 e2.grid(row = 1, column = 1, pady = 20, padx = 10)
@@ -154,41 +142,75 @@ e2.grid(row = 1, column = 1, pady = 20, padx = 10)
 e3 = Entry(master, width = 10)
 e3.grid(row = 2, column = 1)
 
-def update_usr(text = ''):
-    status.set( text + '\n\n' + status.get())
 
-Button(master, text='Link', command=show_entry_fields).grid(row=3, column=1, pady=20)
-Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, padx = 75, pady=20)
+w = Canvas(master, width = 120, height=15)
+w.grid(row=4, column=1, pady = 0)
 
 status = StringVar()
-Label(master, textvariable=status).grid(row=4, column=1,pady = 0)
+f = Label(w, textvariable=status)
 
-status.set("""
-Warnings are printed if .tif files for one spot do not contain \n designated number of stains (leave blank for no warning)""")
-update_usr('________Num of Stains________')
+scrollbar = Scrollbar(w)
+scrollbar.pack(side=RIGHT, fill=Y)
+
+listbox = Listbox(w, width = 120, height = 15)
+listbox.pack()
+
+listbox.config(yscrollcommand=scrollbar.set)
+scrollbar.config(command=listbox.yview)
 
 
-update_usr(
-""".afi file displays this path to HALO, use directory mask if HALO is on another machine \n
-user must conclude path with '/' or '\\' depending on Operating System, \n
-the path also need to end with the folder containing the images.
-"""
-)
-update_usr('________Directory Mask________')
+def update_usr(listbox, text = '', end = False ):
+    if end:
+        listbox.insert(END, '    ' + text)
+        listbox.insert(END, ' ')
+    else:
+        listbox.insert(ACTIVE, '    ' + text)
+        listbox.insert(ACTIVE, ' ')
+def show_entry_fields(listbox):
+   ''' calls afi writing functions according to user input into GUI
+        e1 corresponds to first field, e2 to second field, so on.
+        traverse( ) takes all user inputs and executes the task, also serves as boolean
+        in case directory does not contain any .tif
+   '''
+   path = os.path.join(e1.get(), '')
+   if not path_exists(path):
+       print(path)
+       update_usr(listbox, 'Not a valid directory path')
+       return
 
-update_usr('''
-afi linking files are written to where linked TIFFs are \n
-In a directory, TIFFs belonging to the same spot are linked\n
-all subdirectories from the path provided are traversed and searched\n
-THIS PROGRAM WILL RE-WRITE EXISTING AFI IF THEY ARE FOUND W/ SAME NAMING SCHEME IN A SUBDIRECTORY
-''')
+   mask_dir = e2.get() #os.path.join(e2.get(), '')
+   print(mask_dir)
 
-update_usr('________General Notes________')
-# status2 = StringVar()
-# Label(master, textvariable=status2).grid(row=5, column=1, pady=20)
-# status2.set('1')
+   num_stains = (e3.get())
+   if num_stains and not num_stains.isdigit():
+       update_usr(listbox, 'Number of Stains not an integer')
+       return
+
+   # file written by link after calling it, link also a bool indicating if tiff found at path
+   if not traverse(path, mask_dir, num_stains):
+       update_usr(listbox, 'No .tif or .tiff found anywhere in %s' %(path))
+       return
+
+Button(master, text='Link', command= lambda: show_entry_fields(listbox)).grid(row=3, column=1, pady=20)
+Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, padx = 75, pady=20)
+
+update_usr(listbox, '________General Notes________', True)
+update_usr(listbox, 'all subdirectories from the path provided are traversed and searched', True)
+update_usr(listbox, 'THIS PROGRAM WILL RE-WRITE EXISTING AFI IF THEY ARE FOUND W/ SAME NAMING SCHEME IN A SUBDIRECTORY', True)
+update_usr(listbox, '''afi linking files are written to where linked TIFFs are''', True)
+update_usr(listbox, 'In a directory, TIFFs belonging to the same spot are linked', True)
+
+update_usr(listbox, '________Num of Stains________', True)
+update_usr(listbox, """Warnings are printed if .tif files for one spot do not contain""", True)
+update_usr(listbox, 'designated number of stains (leave blank for no warning)', True)
+
+# update_usr(listbox, """.afi file displays this path to HALO, use directory mask if HALO is on another machine """, True)
+#
+# update_usr(listbox, 'the path also need to end with the folder containing the images.', True)
+# update_usr(listbox, 'user must conclude path with backslash or forward slash depending on Operating System,', True)
+
+# update_usr(listbox, '________Directory Mask________', True )
+#
+#
 
 mainloop( )
-
-# TODO
-# mask directory
