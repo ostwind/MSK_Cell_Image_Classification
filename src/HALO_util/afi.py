dd2438690b8fc3ace720ea913ce999c3551689c4
ostwind <lihan.yao@gmail.com>
2017-08-31 14:39:29 -0400

    afi file now more verbose and bigger. report updated

--- src/HALO_util/afi.py
+++ src/HALO_util/afi.py
@@ -23,12 +23,6 @@ def collect_spots(path):
                 spot_dict[spot] = [f]
     return spot_dict
 
-def case_name(path):
-    parent_dir = path.split('/')[-2]
-    if '_' not in parent_dir:
-        return parent_dir
-    return parent_dir #parent_dir.split('_')[0] + '_' + parent_dir.split('_')[1]
-
 def channel_name(file_name):
     file_name_in_list = file_name.split('_')
     cname = file_name_in_list[0]
@@ -37,8 +31,15 @@ def channel_name(file_name):
         cname += '_' + file_name_in_list[1]
     if  len(file_name.split('_')[2]) < 3:
         cname += '_' + file_name_in_list[2]
+
     if 'dapi' in file_name:
-        return 'DAPI%s'%( int(cname[1:]) )
+        try:
+            return 'DAPI%s'%( int(cname[1:]) )
+        except ValueError:
+            update_usr(listbox, '!!!!!!!!!!!!!!!!!')
+            update_usr(listbox, 'bad dapi filename (first part w/ num value) : %s ' %(cname))
+            update_usr(listbox, '!!!!!!!!!!!!!!!!!')
+
     return cname
 
 def write_afi(dirpath, mask_dir, spot, tif_ledger):
@@ -52,10 +53,23 @@ def write_afi(dirpath, mask_dir, spot, tif_ledger):
         @param dic tif_ledeger: keys of this dic are spots, provides a list of filenames
         which belong to this spot
     '''
-    # read S[year num]_[5 digit serial]_Spot[spot num]
-    filename = '%s_Spot%s.afi' %( case_name(dirpath),  int(spot))
-    root = ET.Element("ImageList")
 
+    def _case_name(path):
+        parent_dir = path.split('/')[-2]
+        return parent_dir
+
+    # read S[year num]_[5 digit serial]_Spot[spot num]
+    try:
+        filename = '%s_Spot%s.afi' %( _case_name(dirpath),  int(spot))
+    except ValueError:
+        update_usr(listbox, '!!!!!!!!!!!!!!!!!')
+        update_usr(listbox, 'first tif in this spot is %s' %(tif_ledger[spot][0]))
+        update_usr(listbox, 'bad .tiff filename at %s , maybe a tif spot is not a number?' %(dirpath))
+        update_usr(listbox, '!!!!!!!!!!!!!!!!!')
+
+
+
+    root = ET.Element("ImageList")
     if mask_dir:
         path_to_write = mask_dir
 
@@ -71,7 +85,9 @@ def write_afi(dirpath, mask_dir, spot, tif_ledger):
         unique_channels.append(channel_name(tif_name))
     # duplicate channel name check
     if len(channel_names_to_sort) != len(set(unique_channels)):
+        update_usr(listbox, '!!!!!!!!!!!!!!!!!')
         update_usr(listbox, 'files with duplicate channel names at %s, skipped! ' %(dirpath) )
+        update_usr(listbox, '!!!!!!!!!!!!!!!!!')
         return False
 
     channel_names_to_sort.sort(key=lambda x: x[0])
@@ -110,6 +126,7 @@ def traverse(start_dir, mask_dir, num_stains):
         tif_ledger = collect_spots(dirpath)
         for spot in tif_ledger.keys():
                 if num_stains and len(tif_ledger[spot]) != int(num_stains):
+                    update_usr(listbox, '!!!!!!!!!!!!!!!!!')
                     update_usr(listbox, 'Spot %s at %s has %s .tif files, no .afi written' %(spot, dirpath, len(tif_ledger[spot])))
                     update_usr(listbox, '!! no .afi written !!' )
                     continue
@@ -117,13 +134,14 @@ def traverse(start_dir, mask_dir, num_stains):
                     update_usr(listbox, 'Spot %s at %s has %s .tif files' %(spot, dirpath, len(tif_ledger[spot])))
 
                 write_afi(dirpath, mask_dir, spot, tif_ledger)
+
     return tif_ledger
 
 def path_exists(path):
     return os.path.exists(os.path.dirname(path))
 
 master = Tk()
-master.geometry('1200x500+300+300')
+master.geometry('1200x800+300+300')
 master.title("HALO's TIFF Linker")
 
 top_label = Label(master, text="Root Directory \n to Traverse" )
@@ -157,7 +175,7 @@ f = Label(w, textvariable=status)
 scrollbar = Scrollbar(w)
 scrollbar.pack(side=RIGHT, fill=Y)
 
-listbox = Listbox(w, width = 120, height = 15)
+listbox = Listbox(w, width = 120, height = 48)
 listbox.pack()
 
 listbox.config(yscrollcommand=scrollbar.set)
@@ -177,6 +195,7 @@ def show_entry_fields(listbox):
         traverse( ) takes all user inputs and executes the task, also serves as boolean
         in case directory does not contain any .tif
    '''
+   update_usr(listbox, '_______________________________________')
    path = os.path.join(e1.get(), '')
    if not path_exists(path):
        update_usr(listbox, 'Not a valid directory path')
@@ -193,6 +212,7 @@ def show_entry_fields(listbox):
    if not traverse(path, mask_dir, num_stains):
        update_usr(listbox, 'No .tif or .tiff found anywhere in %s' %(path))
        return
+
 
 Button(master, text='Link', command= lambda: show_entry_fields(listbox)).grid(row=3, column=1, pady=20)
 Button(master, text='Quit', command=master.quit).grid(row=3, column=0, sticky=W, padx = 75, pady=20)
